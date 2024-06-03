18238a8bf7d963d71c7be5e97a54d78cf0249c4d
dlwlgusdev <plus3456789@naver.com>
2024-06-03 17:21:28 +0900

    feat : chart 생성

---
+++ src/lib/components/chart.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { Chart, registerables } from "chart.js";
+  import { data } from "./data";
+
+  let canvas: HTMLCanvasElement;
+
+  Chart.register(...registerables); // Chart.js 모듈을 등록합니다.
+
+  onMount(() => {
+    const ctx = canvas.getContext("2d");
+    if (ctx) {
+      new Chart(ctx, {
+        type: "line", // 차트 유형을 변경할 수 있습니다 (예: 'bar', 'line', 'pie', 등)
+        data: data,
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+        },
+      });
+    }
+  });
+</script>
+
+<canvas bind:this={canvas}></canvas>
