43207a6fde1ff243806cd98c38d4338303622a1f
plus-test <plus121798@naver.com>
2024-06-14 16:57:58 +0900

    chart 수정

--- src/lib/components/chart.svelte
+++ src/lib/components/chart.svelte
@@ -1,28 +1,38 @@
-<script lang="ts">
-  import { onMount } from "svelte";
-  import { Chart, registerables } from "chart.js";
-  import { data } from "./data";
+import { onMount } from "svelte";
+import { Chart, registerables } from "chart.js";
+import { data } from "./data";
 
-  let canvas: HTMLCanvasElement;
+let chart: Chart | null = null;
 
-  Chart.register(...registerables); // Chart.js 모듈을 등록합니다.
+onMount(() => {
+  Chart.register(...registerables);
 
-  onMount(() => {
-    const ctx = canvas.getContext("2d");
-    if (ctx) {
-      new Chart(ctx, {
-        type: "line", // 차트 유형을 변경할 수 있습니다 (예: 'bar', 'line', 'pie', 등)
-        data: data,
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true,
-            },
-          },
+  const ctx = document.getElementById("myChart") as HTMLCanvasElement;
+
+  chart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["Red", "Blue", "Yellow", "Green"],
+      datasets: [
+        {
+          label: "# of Votes",
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1,
         },
-      });
-    }
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
   });
+});
 </script>
 
-<canvas bind:this={canvas}></canvas>
+<div style="width: 800px;">
+<canvas id="myChart"></canvas>
+</div>
+
