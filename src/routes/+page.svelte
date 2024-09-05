9810a9ed6ae50ae52c50b025d3c4ee8c8930ca15
devcomfort <kimdonghyun026@gmail.com>
2024-09-05 20:52:37 +0000

    feat : 전반적인 코드 로직, 가독성 개선(주석 추가)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,50 +1,43 @@
 <script lang="ts">
-  import { totalNumberStore } from "../lib/components/store";
-  import { Button } from "flowbite-svelte";
-  import { Datepicker } from "flowbite-svelte";
-  type Tag = "월급" | "용돈" | "기타";
+  import moment from "moment";
+  import { onMount } from "svelte";
+  import { Button, Datepicker } from "flowbite-svelte";
 
-  interface Data {
-    /** 생성 시간 */
-    date: any;
-    /** 액수 */
-    money: number;
-    tag: Tag;
-    /** 비고 */
-    description: string;
-    /**총 금액*/
-    totalmoney: number;
-  }
+  import {
+    ExpenseTagSchema,
+    IncomeTagSchema,
+    MiscTagSchema,
+    type ExpenseTag,
+    type IncomeTag,
+    type MiscTag,
+  } from "$lib/schemas";
+  import { recordHandler } from "$lib/store/records";
+
+  /** Moment.js 사용을 위한 날짜 스키마 */
+  const DATE_SCHEMA = "yyyy/MM/DD";
+  // 기본값: 현재 날짜를 DATE_SCHEMA에 맞게 포메팅한 문자열
   let dates: string[] = [];
-  let datas: Data[] = [];
-  let selectedOption: "" | "월급" | "용돈" | "기타" = ""; // 선택된 옵션을 저장할 변수
-  let incomeNumber: number;
-  let totalNumber = $totalNumberStore;
 
   $: {
     console.log(dates);
   }
 
-  // 컴포넌트가 처음 로드될 때 실행되는 함수
-  // onMount(() => {
-  //   totalNumberStore.set(0); // totalNumber 초기화
-  //   totalNumber = 0;
-  // });
+  let selectedOption: IncomeTag | ExpenseTag | MiscTag | "" = ""; // 선택된 옵션을 저장할 변수
+  /**
+   * ### `input` 태그에 입력된 숫자 값
+   *
+   * 소득, 지출 여부를 아직 모름
+   */
+  let inputNumber = 0;
 
-  //수입 지출 계산하는 함수
-  function calculate() {
-    if (totalNumber === 0) {
-      totalNumber = incomeNumber;
-    } else {
-      totalNumber = totalNumber + incomeNumber;
-    }
-    totalNumberStore.set(totalNumber);
-  }
+  onMount(() => {
+    dates = [moment().format(DATE_SCHEMA)];
+  });
 
   // 선택된 옵션과 현재 날짜 및 시간을 배열에 추가하는 함수
   function addSelectedOption() {
     // 정수 여부 검증
-    if (!Number.isSafeInteger(incomeNumber)) {
+    if (!Number.isSafeInteger(inputNumber)) {
       window.alert("수익을 숫자로 입력해주세요!");
       return;
     }
@@ -54,18 +47,14 @@
       return;
     }
 
-    calculate();
-
-    datas = [
-      ...datas,
-      {
-        date: dates,
-        description: "",
-        tag: selectedOption || "기타",
-        money: incomeNumber,
-        totalmoney: totalNumber,
-      },
-    ];
+    recordHandler.add({
+      // `DATE_SCHEMA`에 따라서 포메팅된 문자열을 사용
+      date: moment().format(DATE_SCHEMA),
+      description: "",
+      tag: selectedOption || "미분류",
+      money: inputNumber,
+      balance: 0,
+    });
 
     selectedOption = ""; // 선택 필드 초기화
   }
@@ -75,22 +64,31 @@
   <div class="input-group">
     <Datepicker
       datepickerButtons={true}
-      datepickerFormat="yyyy/mm/dd"
+      datepickerFormat={DATE_SCHEMA}
       bind:values={dates}
     />
-    <input type="number" placeholder="수입 금액" bind:value={incomeNumber} />
+    <input type="number" placeholder="수입 금액" bind:value={inputNumber} />
     <select bind:value={selectedOption}>
       <option value="" disabled>옵션 선택</option>
-      <option value="월급">월급</option>
-      <option value="용돈">용돈</option>
-      <option value="기타">기타</option>
+      <!-- 수입 옵션들 -->
+      {#each Object.values(IncomeTagSchema.Values) as item}
+        <option value={item}>{item}</option>
+      {/each}
+      <!-- 지출 옵션들 -->
+      {#each Object.values(ExpenseTagSchema.Values) as item}
+        <option value={item}>{item}</option>
+      {/each}
+      <!-- 미분류 옵션들 -->
+      {#each Object.values(MiscTagSchema.Values) as item}
+        <option value={item}>{item}</option>
+      {/each}
     </select>
     <Button color="blue" on:click={addSelectedOption}>추가</Button>
   </div>
   <ul>
-    {#each datas as { date, description, tag, money, totalmoney }}
+    {#each $recordHandler as { date, description, tag, money, balance }}
       <li>
-        <p>{date} : 수입 {money}원 {tag} {description} 총액 : {totalmoney}</p>
+        <p>{date} : {money}원 {tag} {description} 잔액 : {balance}</p>
       </li>
     {/each}
   </ul>
