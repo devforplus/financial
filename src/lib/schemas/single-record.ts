8c3523a61c275ef9e2dcd0da695909ee3a7dddae
devforplus <dlwlgusdev@gmail.com>
2024-09-08 21:50:58 +0900

    feat : 수익,지출 탭 추가 및 처리 전 잔액 추가

--- src/lib/schemas/single-record.ts
+++ src/lib/schemas/single-record.ts
@@ -3,6 +3,7 @@ import z from "zod";
 import { IncomeTagSchema } from "./income-tag";
 import { ExpenseTagSchema } from "./expense-tag";
 import { MiscTagSchema } from "./misc-tag";
+import { balance } from "$lib/store/balance";
 
 export const SingleRecordSchema = z.object({
 	// 처리된 날짜
@@ -11,11 +12,13 @@ export const SingleRecordSchema = z.object({
 	// 입출금 액수: 정수로 제한
 	money: z.number().int(),
 	// 처리 전 잔액
-
+	balance: z.number().int(),
 	// 설명
 	description: z.string(),
-
+	//옵션 선택 
 	tag: z.union([IncomeTagSchema, ExpenseTagSchema, MiscTagSchema]),
+	//수익, 지출 탭
+	tab: z.string(),
 });
 
 /** 가계부용 개별 기록 */
